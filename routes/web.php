451e3d62307e1a9401cb8a519e5b847fd4690adb
Francis <71037359+lightdcreator@users.noreply.github.com>
2023-07-13 16:32:55 +0800

    Backend Portfolio Page Setup Part 2

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,7 @@ Route::controller(AboutController::class)->group(function () {
     Route::post('/update/about', 'UpdateAbout')->name('update.about');
     Route::get('/about', 'HomeAbout')->name('home.about');
 
-    Route::get('/about/multi/image','AboutMultiImage')->name('about.multi.image');
+    Route::get('/about/multi/image', 'AboutMultiImage')->name('about.multi.image');
     Route::post('/about/multi/image', 'StoreMultiImage')->name('store.multi.image');
 
     Route::get('/all/multi/image', 'AllMultiImage')->name('all.multi.image');
@@ -50,17 +50,18 @@ Route::controller(AboutController::class)->group(function () {
     Route::post('/update/multi/image', 'UpdateMultiImage')->name('update.multi.image');
 
     Route::get('/delete/multi/image/{id}', 'DeleteMultiImage')->name('delete.multi.image');
-
 });
 
 
 
 //Portfolio
 
-Route::controller(PortfolioController::class)->group(function(){
+Route::controller(PortfolioController::class)->group(function () {
 
-Route::get('/all/portfolio','AllPortfolio')->name('all.portfolio');
+    Route::get('/all/portfolio', 'AllPortfolio')->name('all.portfolio');
 
+    Route::get('/add/portfolio', 'AddPortfolio')->name('add.portfolio');
+    Route::post('/store/portfolio', 'StorePortfolio')->name('store.portfolio');
 });
 
 
