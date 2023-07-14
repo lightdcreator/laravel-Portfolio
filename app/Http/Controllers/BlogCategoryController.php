6f79dfa28a230dc2757e9bf8ecf1b8fd786674e2
Francis <71037359+lightdcreator@users.noreply.github.com>
2023-07-14 14:48:05 +0800

    Backend Blog Category Setup Part 2

--- app/Http/Controllers/BlogCategoryController.php
+++ app/Http/Controllers/BlogCategoryController.php
@@ -14,4 +14,39 @@ class BlogCategoryController extends Controller
         $blogcategory = BlogCategory::latest()->get();
         return view('admin.blog_category.blog_category_all', compact('blogcategory'));
     } // End Method
+
+
+
+    public function AddBlogCategory()
+    {
+
+        return view('admin.blog_category.blog_category_add');
+    } // End Method
+
+
+    public function StoreBlogCategory(Request $request)
+    {
+
+        $request->validate([
+            'blog_category' => 'required'
+
+        ], [
+
+            'blog_category.required' => 'Blog Cateogry Name is Required',
+
+        ]);
+
+
+        BlogCategory::insert([
+            'blog_category' => $request->blog_category,
+
+        ]);
+
+        $notification = array(
+            'message' => 'Blog Category Inserted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.blog.category')->with($notification);
+    } // End Method
 }
--- routes/web.php
+++ routes/web.php
@@ -75,6 +75,10 @@ Route::controller(PortfolioController::class)->group(function () {
 
 Route::controller(BlogCategoryController::class)->group(function () {
     Route::get('/all/blog/category', 'AllBlogCategory')->name('all.blog.category');
+
+    Route::get('/add/blog/category', 'AddBlogCategory')->name('add.blog.category');
+
+    Route::post('/store/blog/category', 'StoreBlogCategory')->name('store.blog.category');
 });
 
 
